3221e09634728bbae4d497f0c0841883e6462d8c
AnsonGit-MakieTech <techmakie@gmail.com>
2025-09-12 12:39:08 +0800

    change and update it

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.login_page, name='login_page'),
     path('register_teacher/', views.register_teacher_page, name='register_teacher_page'),
+    path('register_student/', views.register_student_page, name='register_student_page'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -8,4 +8,7 @@ def login_page(request):
     return render(request, 'authen/login_page.html')
 
 def register_teacher_page(request):
-    return render(request, 'authen/register_teacher.html')
+    return render(request, 'authen/register_students.html')
+
+def register_student_page(request):
+    return render(request, 'authen/register_teachers.html')
