b6cf283b0f95c16121f0904762bc950e719ffb04
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-12-07 21:31:54 +0800

    migrate to redis

--- LAPYXS/settings.py
+++ LAPYXS/settings.py
@@ -149,9 +149,18 @@ AUTH_USER_MODEL = 'backend.CustomUser'
 
 # WEBSOCKET
 WS_GROUPNAME = "LAPYXS"
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",
+#     },
+# }
+
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 
