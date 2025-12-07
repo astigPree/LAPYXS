3389cdbb28848e5a6fc32664ace140571715dc89
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-12-07 19:57:03 +0800

    add daphne

--- LAPYXS/settings.py
+++ LAPYXS/settings.py
@@ -38,7 +38,7 @@ CORS_ORIGIN_ALLOW_ALL = True # For testing
 # Application definition
 
 INSTALLED_APPS = [
-    # 'daphne',
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
